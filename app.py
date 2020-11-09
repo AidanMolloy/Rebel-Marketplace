82e983b1dfeed74a3cce98415be28e45d1854bac
ap7t <adampurcell7t@gmail.com>
2020-11-09 19:19:03 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,90 @@
+from flask import Flask,  render_template, url_for, request
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "0748d28ecb03830a0acae6d1886c55ed"
+
+products = [
+    {
+        "id": 0,
+        "name": "Sample product 1",
+        "company": "Sample Company 1",
+        "price": 20,
+    },
+    {
+        "id": 1,
+        "name": "Sample product 2",
+        "company": "Sample Company 2",
+        "price": 69,
+    },
+    {
+        "id": 2,
+        "name": "Sample product 3",
+        "company": "Sample Company 3",
+        "price": 420,
+    },
+    {
+        "id": 3,
+        "name": "Sample product 4",
+        "company": "Sample Company 4",
+        "price": 50,
+    },
+    {
+        "id": 4,
+        "name": "Sample product 5",
+        "company": "Sample Company 5",
+        "price": 20,
+    },
+    {
+        "id": 5,
+        "name": "Sample product 6",
+        "company": "Sample Company 6",
+        "price": 69,
+    },
+    {
+        "id": 6,
+        "name": "Sample product 7",
+        "company": "Sample Company 7",
+        "price": 420,
+    },
+    {
+        "id": 7,
+        "name": "Sample product 8",
+        "company": "Sample Company 8",
+        "price": 50,
+    }
+]
+
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/product/<int:pid>")
+def product(pid):
+    return render_template("products.html", product=products[pid])
+
+@app.route("/catalog/")
+def catalog():
+    return render_template("catalog.html", products=products)
+
+@app.route("/register/", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        email = request.form.get("email")
+        psw = request.form.get("psw")
+        print(email, psw)
+    return render_template("register.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
